bc216a70fe345d8a4038a927997f87e684b4eddf
hhhejiaman <1258394564@qq.com>
2020-01-10 20:02:50 +0800

    [he]{feat}set different colors for different status

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var apiUrl = 'http://localhost:3000/projects';
 var listBody = document.getElementById('list-body');
 var html = "";
-console.log(listBody)
 
 function updataList(event) {
     for (i = 0; i < event.length; i++) {
@@ -9,11 +8,29 @@ function updataList(event) {
         html += `<td>${event[i].name}</td>`;
         html += `<td>${event[i].description}</td>`;
         html += `<td>${event[i].endTime}</td>`;
-        html += `<td>${event[i].status}</td>`;
+        html += `<td class="item-status">${event[i].status}</td>`;
         html += "<td><div>删除</div></td>";
         html += "</tr>";
     }
     listBody.innerHTML = html;
+}
+
+function colorCheck(event) {
+    var itemStatus = document.getElementsByClassName("item-status");
+
+    for (i = 0; i < event.length; i++) {
+        switch (itemStatus[i].innerHTML) {
+            case 'ACTIVE':
+                itemStatus[i].style.color = '#666666';
+                break;
+            case 'PENDING':
+                itemStatus[i].style.color = '#ee706d';
+                break;
+            case 'CLOSED':
+                itemStatus[i].style.color = '#f7da47';
+                break;
+        }
+    }
 }
 
 function getItemData() {
@@ -22,6 +39,7 @@ function getItemData() {
         method: 'GET',
         success: function(res) {
             updataList(res);
+            colorCheck(res);
         },
         fail: function(error) {
             console.log('error occurred')
